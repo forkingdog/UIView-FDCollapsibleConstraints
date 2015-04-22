9f1cb113fd42e378e04cfcc87372fccebdc17918
Yuan.Sun <sunyuan1713@gmail.com>
2015-04-22 09:18:39 +0800

    Auto collapse

--- Classes/UIView+FDCollapsibleConstraints.h
+++ Classes/UIView+FDCollapsibleConstraints.h
@@ -22,32 +22,51 @@
 
 #import <UIKit/UIKit.h>
 
-/// `UIView+FDCollapsibleConstraints` helps to implement flow-layout-like behavior
+/// "UIView+FDCollapsibleConstraints" helps to implement flow-layout-like behavior
 /// in Auto Layout (with Interface Builder).
 ///
 /// Normally, when a view is to be hidden (which is common in type multiplexing
 /// views or cells), some related constraints in horizontal or vertical direction
-/// should also be disabled (by setting the `constant` property to zero as we might
+/// should also be disabled (by setting the "constant" property to zero as we might
 /// want to reuse the view later) to avoid double margin.
 ///
 /// Now with this extension, you could easily achieve this by specifying constraints
-/// to be set to zero when the view is `collapsed`, as well as setting the
-/// `fd_collapsed` property when you set the view hidden or with empty content.
+/// to be set to zero when the view is "collapsed", as well as setting the
+/// "fd_collapsed" property when you set the view hidden or with empty content.
 ///
 /// For UIView's with fixed height constraint, in most cases you should use it
 /// with its height constraint and top (or bottom) margin constraint connected
 /// to fd_collapsibleConstraints IBOutletCollection. For UILabel-like intrinsic
 /// content size based views, you typically don't have a height constraint, thus
 /// only need to connect its margin constraint. You'll get the label totally
-/// disappeared by setting its `title` to nil and `fd_collapsed` to YES.
+/// disappeared by setting its "title" to nil and "fd_collapsed" to YES.
 @interface UIView (FDCollapsibleConstraints)
 
 /// Assigning this property immediately disables the view's collapsible constraints'
 /// by setting their constants to zero.
 @property (nonatomic, assign) BOOL fd_collapsed;
 
-/// Specify constraints to be affected by `fd_collapsed` property by connecting in
+/// Specify constraints to be affected by "fd_collapsed" property by connecting in
 /// Interface Builder.
 @property (nonatomic, copy) IBOutletCollection(NSLayoutConstraint) NSArray *fd_collapsibleConstraints;
+
+@end
+
+@interface UIView (FDAutomaticallyCollapseByIntrinsicContentSize)
+
+/// Enable to automatically collapse constraints in "fd_collapsibleConstraints" when
+/// you set or indirectly set this view's "intrinsicContentSize" to {0, 0} or absent.
+///
+/// For example:
+///  imageView.image = nil;
+///  label.text = nil, label.text = @"";
+///
+/// "NO" by default, you may enable it by codes.
+@property (nonatomic, assign) BOOL fd_autoCollapse;
+
+/// "IBInspectable" property, more friendly to Interface Builder.
+/// You gonna find this attribute in "Attribute Inspector", toggle "On" to enable.
+/// Why not a "fd_" prefix? Xcode Attribute Inspector will clip it like a shit.
+@property (nonatomic, assign, getter=fd_autoCollapse) IBInspectable BOOL autoCollapse;
 
 @end
--- Classes/UIView+FDCollapsibleConstraints.m
+++ Classes/UIView+FDCollapsibleConstraints.m
@@ -44,11 +44,11 @@
 
 @implementation UIView (FDCollapsibleConstraints)
 
-#pragma mark - KVC Hack
+#pragma mark - Hacking KVC
 
 + (void)load
 {
-    // swizzle setValue:forKey: to intercept assignments to `fd_collapsibleConstraints`
+    // Swizzle setValue:forKey: to intercept assignments to `fd_collapsibleConstraints`
     // from Interface Builder. We should not do so by overriding setvalue:forKey:
     // as the primary class implementation would be bypassed.
     SEL originalSelector = @selector(setValue:forKey:);
@@ -65,15 +65,16 @@
 {
     NSString *injectedKey = [NSString stringWithUTF8String:sel_getName(@selector(fd_collapsibleConstraints))];
     if ([key isEqualToString:injectedKey]) {
-        // hook assignments to our custom `fd_collapsibleConstraints` property.
+        
+        // Hook assignments to our custom `fd_collapsibleConstraints` property.
         NSMutableArray *constraints = (NSMutableArray *)self.fd_collapsibleConstraints;
-        [value enumerateObjectsUsingBlock:
-         ^(NSLayoutConstraint *constraint, NSUInteger idx, BOOL *stop) {
+        [(NSArray *)value enumerateObjectsUsingBlock:^(NSLayoutConstraint *constraint, NSUInteger idx, BOOL *stop) {
+             // Store original constant value
              constraint.fd_originalConstant = constraint.constant;
              [constraints addObject:constraint];
         }];
     } else {
-        // forward the rest of KVC's to original implementation.
+        // Forward the rest of KVC's to original implementation.
         [self fd_setValue:value forKey:key];
     }
 }
@@ -90,7 +91,7 @@
              constraint.constant = constraint.fd_originalConstant;
          } 
      }];
-    
+
     objc_setAssociatedObject(self, @selector(fd_collapsed), @(collapsed), OBJC_ASSOCIATION_RETAIN);
 }
 
@@ -113,6 +114,67 @@
 {
     NSMutableArray *constraints = (NSMutableArray *)self.fd_collapsibleConstraints;
     [constraints setArray:fd_collapsibleConstraints];
+}
+
+@end
+
+@implementation UIView (FDAutomaticallyCollapseByIntrinsicContentSize)
+
+#pragma mark - Hacking "-updateConstraints"
+
++ (void)load
+{
+    // Swizzle to hack "-updateConstraints" method
+    SEL originalSelector = @selector(updateConstraints);
+    SEL swizzledSelector = @selector(fd_updateConstraints);
+    
+    Class class = UIView.class;
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+    
+    method_exchangeImplementations(originalMethod, swizzledMethod);
+}
+
+- (void)fd_updateConstraints
+{
+    // Call primary method's implementation
+    [self fd_updateConstraints];
+ 
+    if (self.fd_autoCollapse && self.fd_collapsibleConstraints.count > 0) {
+        
+        // "Absent" means this view doesn't have an intrinsic content size, {-1, -1} actually.
+        const CGSize absentIntrinsicContentSize = CGSizeMake(UIViewNoIntrinsicMetric, UIViewNoIntrinsicMetric);
+        
+        // Calculated intrinsic content size
+        CGSize contentSize = [self intrinsicContentSize];
+        
+        // When this view doesn't have one, or has no intrinsic content size after calculating,
+        // it going to be collapsed.
+        if (CGSizeEqualToSize(contentSize, absentIntrinsicContentSize) ||
+            CGSizeEqualToSize(contentSize, CGSizeZero)) {
+            self.fd_collapsed = YES;
+        } else {
+            self.fd_collapsed = NO;
+        }
+    }
+}
+
+#pragma mark - Dynamic Properties
+
+- (BOOL)fd_autoCollapse
+{
+    return [objc_getAssociatedObject(self, _cmd) boolValue];
+}
+
+- (void)setFd_autoCollapse:(BOOL)autoCollapse
+{
+    objc_setAssociatedObject(self, @selector(fd_autoCollapse), @(autoCollapse), OBJC_ASSOCIATION_RETAIN);
+}
+
+- (void)setAutoCollapse:(BOOL)collapse
+{
+    // Just forwarding
+    self.fd_autoCollapse = collapse;
 }
 
 @end
